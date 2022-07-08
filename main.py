b459f9324fdcdc0267a24a472bc3262b8cc2990d
Szybki0o0 <105429743+Szybki0o0@users.noreply.github.com>
2022-07-08 21:21:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,18 +11,33 @@ def SettingWindow():
 
 def StartingWindow():
     isRunning = True
+    activeCoordinateLeft = 0
+    activeCoordinateTop = 0
     pygame.display.flip()
     while isRunning:
         # print(pygame.mouse.get_pos())
-        # Updates frames of rectangles
+        # Updates frames of rectangles changing color to orange
+        # and on mouse away changing back to white rect
         for element in rectList:
-            if element.collidepoint(pygame.mouse.get_pos()):
-                pygame.draw.rect(screen, orange, pygame.Rect(element.left,element.top,40,40),3)
+            if element.left == activeCoordinateLeft and element.top == activeCoordinateTop:
+                pygame.draw.rect(screen, red, pygame.Rect(element.left, element.top, 40, 40), 3)
                 pygame.display.update()
             else:
-                pygame.draw.rect(screen, white, pygame.Rect(element.left, element.top, 40, 40))
-        # Makes closing window available
+                if element.collidepoint(pygame.mouse.get_pos()):
+                    pygame.draw.rect(screen, orange, pygame.Rect(element.left,element.top,40,40),3)
+                    pygame.display.update()
+                else:
+                    pygame.draw.rect(screen, white, pygame.Rect(element.left, element.top, 40, 40))
+        # Activates rectangle changing color to red
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for element in rectList:
+                    if element.collidepoint(pygame.mouse.get_pos()):
+                        activeCoordinateLeft = element.left
+                        activeCoordinateTop = element.top
+                        pygame.draw.rect(screen, red, pygame.Rect(element.left, element.top, 40, 40), 3)
+                        pygame.display.update()
+            # Makes closing window available
             if event.type == pygame.QUIT:
                 isRunning = False
             if isRunning == False:
@@ -32,12 +47,15 @@ def SettingColors():
     global orange
     global white
     global red
+    global black
+    black = (0,0,0)
     red = (255,0,0)
     orange = (255,165,0)
     white = (255, 255, 255)
 
 SettingColors()
 SettingWindow()
+
 def DrawingBoard():
     global rectList
     rectList = []
@@ -45,7 +63,7 @@ def DrawingBoard():
     y = 40
     inside = 42
     devide = 44
-    # Draws and adds rectangles to list
+    # Draws from right to left and adds rectangles to list the same way
     for col in range(1,10):
         for row in range(1,9):
             if row == 1:
@@ -66,7 +84,16 @@ def DrawingBoard():
             rectList.append(pygame.Rect(x, y, 40, 40))
             y+=inside
         x = 40
-    print(rectList)
+
+    # I know something higher is wrong and it adds 9 more rectangles but
+    # I created this wonderful piece of code that removes it :)
+    top = 0
+    for el in rectList:
+        if el.left == 40 and el.top == top:
+            rectList.remove(el)
+        else:
+            top = el.top
+
 DrawingBoard()
 
 StartingWindow()
